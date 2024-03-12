2579d8ad0cfa2ba26f246fe080f84c8238d8ab30
Minadarabi <minaadabirii@gmail.com>
2024-03-12 11:13:35 -0700

    last commit

--- src/Components/Qr.jsx
+++ src/Components/Qr.jsx
@@ -5,15 +5,21 @@ import "./style.css"
 
 export const Qr = () => {
     const [input , setInput] = useState("")
-    const [qr , setQr] = useState("")
+    const [qr , setQr] = useState(null);
+
+    function generateQr(){
+        setQr(input)
+        setInput("")
+        
+    }
   return (
     <>
      <div>
         <h1>QR Code Generator</h1>
         <div className="qr-container">
-          <input onChange={(e) => setInput(e.target.value)} type="text" placeholder="Enter Your Value here" />
-          <button>Generate</button>
-          <QRCode value="ghjl" title="gbhnjm" size={300} />    
+          <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Enter Your Value here" />
+          <button onClick={generateQr}>Generate</button>
+          {!!qr && <QRCode value={qr} title="QR" size={300} /> }   
 
         </div>
       
